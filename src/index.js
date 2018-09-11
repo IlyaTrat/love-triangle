551795ef15aa5a31fe11b27170defae95aa4cef2
Трацевский Илья <deeo-sama@hotmail.com>
2018-09-11 18:00:06 +0300

    Done with love

--- src/index.js
+++ src/index.js
@@ -3,5 +3,14 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  let count = 0;
+  for (let index = 0; index < preferences.length; index++) {
+    let bIndex = preferences[index] - 1;
+    let cIndex = preferences[bIndex] - 1;
+    let aIndex = preferences[cIndex] - 1;
+    if(aIndex == index && !(bIndex == cIndex || bIndex == aIndex)) {
+      count ++;
+    }
+  }
+  return count / 3;
 };
